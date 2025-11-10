9dfd5b76e477210885dc504eeb9284c8ea358aed
FrancescoTroise <francesco.troise98@gmail.com>
2025-11-10 17:51:14 +0100

    Milestone 2
    Add the ability to delete items

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
-
+import SectionAddArticle from "./components/SectionAddArticle";
 import ListArticles from "./components/ListArticles";
+import { useState } from "react";
+//Import di "useState" per aver accesso ad uno "state" che accolga l'array articles
 
 function App() {
-  const articles = [
+  const array_articles = [
     {
       id: "A1",
       id_corso: "Corso Full-stack Web Development -FULL TIME-",
@@ -33,13 +35,40 @@ function App() {
         "Stellae bellum gerunt, heroēs fortiter adversus tenebras pugnant. Per aspera ad astra",
     },
   ];
+  //Array di dati
+
+  const [articles, setArticles] = useState(array_articles);
+  //Dichiarazione dello "state"
+
+  function handle_trash(key) {
+    const filtered_articles = articles.filter((art) => art.id != key);
+    setArticles(filtered_articles);
+  }
+  //"handle_trash" accetta l'id corrispondente all'oggetto a cui è legato il button
+  //filtra poi lo state originario, conservando tutto, tranne l'oggeto con quell'id
+  //cambia infine lo stato con il nuovo array filtrato
 
   return (
     <>
       <Header />
 
+      <SectionAddArticle />
+
       {articles.map((art) => (
-        <ListArticles {...art} />
+        <div className="wrapper" key={art.id}>
+          <ListArticles {...art} />
+          {/* Il tag "ListArticles" riceve lo spred del singolo oggetto "art" */}
+
+          <button
+            className="btn btn-sm btn-danger"
+            onClick={() => {
+              handle_trash(art.id);
+              //Al click del button richiama la funzione "handle_trash" che accetto l'id del singolo oggetto "art"
+            }}
+          >
+            <i className="bi bi-trash"></i>
+          </button>
+        </div>
       ))}
     </>
   );
---
+++ src/components/SectionAddArticle.jsx
@@ -0,0 +1,11 @@
+export default function SectionAddArticle() {
+  return (
+    <>
+      <button type="submit">Add Article</button>
+      <form>
+        <label htmlFor="input-article">Add Article: </label>
+        <input type="text" id="input-article"></input>
+      </form>
+    </>
+  );
+}
